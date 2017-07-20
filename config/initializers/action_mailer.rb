a515d0e39587e6540e20a684875457c1882c7883
Angelo Delefortrie <angelo.delefortrie@gmail.com>
2017-07-20 13:17:04 +0200

    Smtp settings not required

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -6,20 +6,22 @@ if Rails.env.development?
     port: 1025
   }
 else
-  # Use smtp configuration defined in /config/lbc.yml
+  # Use smtp configuration defined in /config/cellar.yml
   ActionMailer::Base.delivery_method       = :smtp
   ActionMailer::Base.perform_deliveries    = true
   ActionMailer::Base.raise_delivery_errors = true
 
-  ActionMailer::Base.smtp_settings = {
-    address:              Settings.emails.smtp.address,
-    port:                 Settings.emails.smtp.port,
-    domain:               Settings.emails.smtp.domain,
-    user_name:            Settings.emails.smtp.user_name,
-    password:             Settings.emails.smtp.password,
-    authentication:       Settings.emails.smtp.authentication,
-    enable_starttls_auto: true
-  }
+  if Settings.has_key?(:emails) && Settings.emails.has_key?(:smtp)
+    ActionMailer::Base.smtp_settings = {
+      address:              Settings.emails.smtp.address,
+      port:                 Settings.emails.smtp.port,
+      domain:               Settings.emails.smtp.domain,
+      user_name:            Settings.emails.smtp.user_name,
+      password:             Settings.emails.smtp.password,
+      authentication:       Settings.emails.smtp.authentication,
+      enable_starttls_auto: true
+    }
+  end
 end
 
 ActionMailer::Base.default_options     = { from: Settings.emails.default_sender }
